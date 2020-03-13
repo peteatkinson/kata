25b162fe15177031815ba72f0db5d69aed26f621
Peter <peteratkinson1994@gmail.com>
2020-03-13 01:51:48 +0000

    Move Offer class file to correct namespace

--- Logic/Offer.cs
+++ Logic/Offer.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Logic.Interfaces
+﻿namespace Logic
 {
-
-
-    
-
     public class Offer
     {
         public string SKU { get; set; }
